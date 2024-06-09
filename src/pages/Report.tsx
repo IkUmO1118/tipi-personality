7afb6339b62c75108159921255a85cd42bffc03d
Ikumo Takahashi <ikumo1118free@gmail.com>
2024-06-09 15:01:10 +0900

    Small change

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,4 +1,4 @@
-import ReportContainer from "@/features/report/ReportContainer";
+import ReportContainer from "../features/report/ReportContainer";
 
 function Report() {
   return <ReportContainer />;
